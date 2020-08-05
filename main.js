434a3ecf9cb7cd83d4a4ec688215ebf3a38b471a
Wal93mec <69161849+Wal93mec@users.noreply.github.com>
2020-08-05 12:53:36 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function(){
+        $("#adjectives").typed({
+            strings: ["musician.", "performer.", "artist.", "Ed Sheeran."],
+            typeSpeed: 15,
+            loop: false,
+            backDelay: 1000
+        });
+    });
